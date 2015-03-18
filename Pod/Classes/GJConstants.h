bdaa8943d336083bd50817c85b2cc4bca6d72f86
Brad Flegel <brad@gravityjack.com>
2015-03-18 13:43:19 -0700

    Simplify is iphone 5 constant.

--- Pod/Classes/GJConstants.h
+++ Pod/Classes/GJConstants.h
@@ -16,9 +16,8 @@
     #define kServerUrl @"https://EXAMPLE.DEVELOPMENT"
 #endif
 
-#define IS_WIDESCREEN_IOS7 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
-#define IS_WIDESCREEN_IOS8 ( fabs( ( double )[ [ UIScreen mainScreen ] nativeBounds ].size.height - ( double )1136 ) < DBL_EPSILON )
-#define IS_IPHONE_5 ( ( [ [ UIScreen mainScreen ] respondsToSelector: @selector( nativeBounds ) ] ) ? IS_WIDESCREEN_IOS8 : IS_WIDESCREEN_IOS7 )
+#define SCREEN_MAX_DIM MAX([[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height)
+#define IS_IPHONE_5 SCREEN_MAX_DIM == 568.0
 #define IS_IPAD  (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 #define IS_OS_7_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
 #define IS_OS_8_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
